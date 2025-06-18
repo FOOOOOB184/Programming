4b9a0317370a751fa9b18f89d5db2282bd90c503
FOOOOOB184 <felixoceanbard@gmail.com>
2025-06-18 19:26:56 -0400

    Start of a game using JOptionPane

--- Programming.java
+++ Programming.java
@@ -5,16 +5,24 @@ import java.awt.*;
 public class Programming {
     public static void main(String[] args) {
 
-        Object[]options = {"Blue","Green"};
 
-        JOptionPane.showOptionDialog(
+       String name = JOptionPane.showInputDialog("Enter Your Name");
+       JOptionPane.showMessageDialog(null,"Hello "+name+",\nL'ets begin your Adventure!");
+
+        Object[] options = {"Blue", "Green"};
+
+        int a = JOptionPane.showOptionDialog(
                 null,
-                "What is your favrite color?",
+                "What is your favorite color?",
                 "Interactions pane",
                 JOptionPane.DEFAULT_OPTION,
                 JOptionPane.ERROR_MESSAGE,
                 null,
                 options,
                 options[0]);
+
+
+
+
     }
 } // End
