7bbaf9c821b64ce5dc199b40aea40c542ed499f8
FOOOOOB184 <felixoceanbard@gmail.com>
2025-06-18 18:39:54 -0400

    Here I made a JOptionPane.SOD to ask the user if they like blue or green.

--- Programming.java
+++ Programming.java
@@ -1,11 +1,25 @@
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+
 public class Programming {
     public static void main(String[] args) {
-        String a = "cool";
-        System.out.println(a);
 
-        if (a == "cool") {
-            System.out.println(a);
-        }
+        Object[]options = {"Blue","Green"};
+
+        JOptionPane.showOptionDialog(
+                null,
+                "What is your favrite color?",
+                "Interactions pane",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.ERROR_MESSAGE,
+                null,
+                options,options[0]);
+
+
+
+
+
 
 
     }
